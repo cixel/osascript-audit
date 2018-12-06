5c81cdc1eada739fe5dfcb79d5ef22855fc29ba7
Ehden Sinai <ehdens@gmail.com>
2018-12-06 10:45:01 -0500

    remove use of osascript.bak
    Writing into /usr/bin/ requires use of csrutil, and likely isn't worth
    the trouble. Things invoking osascript by full path will get around this
    wrapper.

--- audit.go
+++ audit.go
@@ -20,7 +20,7 @@ func main() {
 
 	log.SetOutput(f)
 
-	osbin, err := exec.LookPath("osascript.bak")
+	osbin, err := exec.LookPath("/usr/bin/osascript")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
